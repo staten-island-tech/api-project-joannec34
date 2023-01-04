9465eb97e6eccdf0e1cbefb97ce21655e451f66a
joannec34 <joannechen9058@gmail.com>
2023-01-04 14:01:01 -0500

    hello

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,1 +1,23 @@
 import "../styles/style.css";
+
+let pokemon = "ditto";
+const pokemonapi = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.name;
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("sad");
+  }
+}
+
+getData(pokemonapi);
